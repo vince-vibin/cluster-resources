792210114954ac890385aa8d31503558ffdf142a
vince-vibin <99386370+vince-vibin@users.noreply.github.com>
2022-04-30 17:46:49 +0200

    added echo statments

--- scripts/prep.sh
+++ scripts/prep.sh
@@ -11,14 +11,32 @@ interface=eth0
 
 SDCardBoot=K: # The SDCard Partition called "boot"
 
+currDate=`date +%D-%T`
+echo "$currDate | script started..."
+
 # enabling cgroup_memory
-echo -n " cgroup_memory=1 cgroup_enable=memory" >> $SDCardBoot/cmdline.txt
+#echo -n "cgroup_memory=1 cgroup_enable=memory" >> $SDCardBoot/cmdline.txt
+
+currDate=`date +%D-%T`
+echo "$currDate | cgroup enabled"
 
 #setting ip, gateway ...
 echo -n " ip=$ip::$gateway:$subnetmask:$hostname:$interface:off" >> $SDCardBoot/cmdline.txt
 
+currDate=`date +%D-%T`
+echo "$currDate | network settings set"
+
 #enabling arm64
 echo "arm_64bit=1" >> $SDCardBoot/config.txt
 
+currDate=`date +%D-%T`
+echo "$currDate | enabled arm64"
+
 #enabling ssh
 touch $SDCardBoot/ssh
+
+currDate=`date +%D-%T`
+echo "$currDate | ssh file generated"
+
+currDate=`date +%D-%T`
+echo "$currDate | script finished your ip is: $ip"
