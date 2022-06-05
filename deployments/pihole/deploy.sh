5900b937380de5b05583f39ea7dda40a45a6c39b
vince-vibin <99386370+vince-vibin@users.noreply.github.com>
2022-06-06 00:36:46 +0200

    copy file magic

---
+++ deployments/pihole/deploy.sh
@@ -0,0 +1,11 @@
+
+
+echo hello
+# getting the repo
+#helm repo add mojo2600 https://mojo2600.github.io/pihole-kubernetes/ 
+#helm repo update
+
+#kubectl create namespace pihole
+
+#helm install your-release mojo2600/pihole
+
