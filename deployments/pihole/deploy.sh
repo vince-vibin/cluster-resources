f920b6fe44bdd4b70569b64eac19d99c03d87cd7
vince-vibin <99386370+vince-vibin@users.noreply.github.com>
2022-06-18 17:57:04 +0200

    sadlökfj

--- deployments/pihole/deploy.sh
+++ deployments/pihole/deploy.sh
@@ -1,11 +1,17 @@
+# fix that worked for me #! do not use for prod enviroments
+# https://stackoverflow.com/a/65755417/18377883
+export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 
-
-echo hello
 # getting the repo
-#helm repo add mojo2600 https://mojo2600.github.io/pihole-kubernetes/ 
-#helm repo update
+helm repo add mojo2600 https://mojo2600.github.io/pihole-kubernetes/ 
+helm repo update
 
-#kubectl create namespace pihole
+kubectl create namespace pihole
 
-#helm install your-release mojo2600/pihole
+# disabling traefic to not conflict with pihole this should be a temporary fix
+# issue open at: https://github.com/MoJo2600/pihole-kubernetes/issues/230
+kubectl delete -n kube-system helmcharts traefik
 
+helm install --version '2.5.8' --namespace pihole --values values.yaml pihole mojo2600/pihole
+# uninstall command:
+# helm uninstall pihole -n pihole
--- deployments/pihole/main.sh
+++ deployments/pihole/main.sh
@@ -1,11 +1,13 @@
 #* This script needs pscp to copy the deploy file to the node 
 #* get pscp here: https://www.chiark.greenend.org.uk/~sgtatham/putty/latest.html
 
+#* before running this set the wished values in values.yaml
+
 masterIP="192.168.178.200"
 masterPassword="raspberry"
 masterUser="pi"
 
-loadbalancerIP="192.168.178.202"
+loadbalancerIP="192.168.178.201"
 piholePassword="0n4BQ2l7Dbu3ViYgd4wu"
 
 function handleError() {
@@ -17,10 +19,10 @@ function handleError() {
 currDate=`date +%D-%T`
 echo "$currDate | connecting to master $masterIP"
 
-sed -i "s|LOAD_BALANCER_SERVER_IP_HERE|$loadbalancerIP|" ./pihole.yaml
-sed -i "s|<your-password-here>|$piholePassword|" ./pihole.yaml
+#sed -i "s|LOAD_BALANCER_SERVER_IP_HERE|$loadbalancerIP|" ./pihole.yaml
+sed -i "s|<your-password-here>|$piholePassword|" ./values.yaml
 
-pscp -q -pw $masterPassword ./pihole.yaml $masterUser@$masterIP:/home/$masterUser || handleError
+pscp -q -pw $masterPassword ./values.yaml $masterUser@$masterIP:/home/$masterUser || handleError
 putty.exe -ssh -pw $masterPassword $masterUser@$masterIP -m ./deploy.sh || handleError
 
 currDate=`date +%D-%T`
